75f20241e55541d194ab72d7832ef7229b5374f1
Viktor Pekin <Viktorpekin43@gmail.com>
2022-08-09 00:47:31 +0700

    test2

--- app.js
+++ app.js
@@ -20,28 +20,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-    }),
-  }),
-  createUser,
-);
-app.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
+app.post('/signup', createUser);
+app.post('/signin', login);
 
 app.use(auth);
 

122ad7a03daecea6931f5031b16524945af47a11
Viktor Pekin <Viktorpekin43@gmail.com>
2022-08-10 12:23:53 +0700

    test 4

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.post(
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
+      avatar: Joi.string(),
     }),
   }),
   createUser,

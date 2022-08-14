149f0f455f4a08fae0cbec13c0f8f58a598e24b8
ViktorPekin <viktorpekin43@gmail.com>
2022-08-14 13:54:22 +0700

    add header cors

--- app.js
+++ app.js
@@ -31,12 +31,13 @@ app.use((req, res, next) => {
 /*   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   } */
-  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Origin', '*');
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     res.end();
   }
 

dac7a96487f449de51fecbcff998d227a575b07c
ViktorPekin <viktorpekin43@gmail.com>
2022-08-14 12:33:54 +0700

    add cors adress

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
-  'localhost:3000',
+  'http://localhost:3000',
 ];
 
 app.use((req, res, next) => {
